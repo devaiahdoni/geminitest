6b297357adb9934932893e2cfcfed727ccee0ae3
devaiahdoni <devaiahdoni@gmail.com>
2025-01-03 20:24:46 -0600

    Update gemini-test.py

--- gemini-test.py
+++ gemini-test.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 google_api_key = os.environ["GOOGLE_API_KEY"]
 
-#genai.configure(api_key="AIzaSyD6YPlN6hwPMvxD3naRYdw3FItpEGC0vsw")
+
 genai.configure(api_key=google_api_key)
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Explain how AI works")
